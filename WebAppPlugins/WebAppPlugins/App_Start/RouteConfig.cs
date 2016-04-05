9c14e45fc417db09c097da508272f1b5f18cfca6
zswcorn <zswcorn@gmail.com>
2016-04-05 22:35:07 +0800

    研究路由

--- WebAppPlugins/WebAppPlugins/App_Start/RouteConfig.cs
+++ WebAppPlugins/WebAppPlugins/App_Start/RouteConfig.cs
@@ -13,12 +13,15 @@ namespace WebAppPlugins
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            var Route = routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ValidatePlugins", action = "TestView", area = "Plugins", id = UrlParameter.Optional },
+                defaults: new { controller = "ValidatePlugins", action = "TestView", id = UrlParameter.Optional },
+                //defaults: new { controller = "ValidatePlugins", action = "TestView", area = "Plugins", id = UrlParameter.Optional },
                 namespaces: new string[] { "WebAppPlugins.Areas.Plugins.Controllers" }
+                //namespaces: new string[] { "WebAppPlugins" }
             );
+            //Route.DataTokens["area"] = "Plugins";
         }
     }
 }
--- WebAppPlugins/WebAppPlugins/Areas/Plugins/PluginsAreaRegistration.cs
+++ WebAppPlugins/WebAppPlugins/Areas/Plugins/PluginsAreaRegistration.cs
@@ -2,22 +2,23 @@
 
 namespace WebAppPlugins.Areas.Plugins
 {
-    public class PluginsAreaRegistration : AreaRegistration 
+    public class PluginsAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Plugins";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Plugins_default",
-                "Plugins/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional },
+                "Plugins/{controller}/{action}/{id}",//"",
+                                                     //new { action = "TestView", id = UrlParameter.Optional }
+                new { controller = "ValidatePlugins", action = "TestView", id = UrlParameter.Optional },
                 new string[] { "WebAppPlugins.Areas.Plugins.Controllers" }
             );
         }
