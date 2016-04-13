7632d133030874c5d5a9932c21d33bbab7957122
zswcorn <zswcorn@gmail.com>
2016-04-13 23:16:59 +0800

    菜单样式及动画效果

---
+++ WebAppPlugins/WebAppPlugins/Scripts/Common/PluginsLayout.js
@@ -0,0 +1,7 @@
+﻿$("document").ready(function () {
+    $(".subMenu").on("click", function (e) {
+        $($(e)[0].currentTarget.children).toggle(function () {
+            $(this).animate({ height: "100%" });
+        });
+    });
+});
